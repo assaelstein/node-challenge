5adb55809f5701f6355377c076b70a8696e314c3
assaelstein <69895279+assaelstein@users.noreply.github.com>
2020-10-22 12:26:16 +0200

    sect1

---
+++ graphQL-basics/src/demo data/index.js
@@ -0,0 +1,18 @@
+
+const users = [
+    {
+        id: '1',
+        name: 'Dovid',
+        email: 'ttt@ttt',
+    },
+    { id: '2', name: 'Shalon', email: 'example@tm.com' },
+]
+
+const posts = [
+    { name: 'Dovid', id: '1', email: 'gm', body: 'the parsha of the week is Lech Lecha' },
+    { name: 'Shaul', id: '2', email: 'yh', body: 'the parsha of the week is VaEira' },
+    { name: 'Shmuel', id: '3', email: 'ol', body: 'the parsha of the week is Noach' },
+]
+
+
+module.exports = { users, posts }
